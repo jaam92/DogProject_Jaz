8bfaa2a89b7be1baff24044a222cadbbafd54f95
jaam92 <jazlyn.mooney@gmail.com>
2019-09-25 14:43:16 -0700

    update window sizes and paths

--- LocalRscripts/InbreedingDepAnalyses/splitAutosomeIntoWindows.R
+++ LocalRscripts/InbreedingDepAnalyses/splitAutosomeIntoWindows.R
@@ -2,16 +2,16 @@
 library(tidyverse)
 
 #read file in and provide a start position
-chroms = read.delim("~/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/chromosomeLengths.txt", stringsAsFactors = F, sep = " ") %>%
+chroms = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/chromosomeLengths.txt", stringsAsFactors = F, sep = " ") %>%
   mutate(startPOS = as.integer(0))
 
 #Break chroms up into (50Kb) steps
 dataList = list()
 for(i in 1:dim(chroms)[1]){
-  ChromStepSize = seq.int(from = chroms[i,3], to = chroms[i,2], by = 50000) %>%
+  ChromStepSize = seq.int(from = chroms[i,3], to = chroms[i,2], by = 5e6) %>%
     as.data.frame() %>%
     dplyr::rename("endPos" = ".") %>%
-    mutate(startPos = endPos - 50000,
+    mutate(startPos = endPos - 5e6,
            chrom = chroms[i,1]) %>%
     filter(startPos >= 0)
   dataList[[i]] = ChromStepSize
@@ -21,4 +21,4 @@ newGeneSet = bind_rows(dataList) %>%
   mutate(startPos = as.integer(startPos),
          endPos = as.integer(endPos))
 
-write.table(newGeneSet, file = "~/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/Autosome_50KbWindows.bed", col.names = F, row.names = F, quote = F, sep = "\t")
+write.table(newGeneSet, file = "~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/Autosome_5MbWindows.bed", col.names = F, row.names = F, quote = F, sep = "\t")
