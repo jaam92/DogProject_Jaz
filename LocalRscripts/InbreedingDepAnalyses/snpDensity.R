69f8a6ae277886f461bf98319c3000a1432f8e5b
jaam92 <jazlyn.mooney@gmail.com>
2019-09-25 18:15:42 -0700

    updated plots

--- LocalRscripts/InbreedingDepAnalyses/snpDensity.R
+++ LocalRscripts/InbreedingDepAnalyses/snpDensity.R
@@ -1,12 +1,13 @@
 #Load Libraries
 library(tidyverse)
 library(GenomicRanges)
-library(ggplot2)
 
 ####Read files in
-genes = read.delim("~/DogProject_Jaz/LocalRscripts/CaseControlROH/EnsemblGenes_CanFam3.1.bed", stringsAsFactors = F)
-gene_names = read.table("~/DogProject_Jaz/LocalRscripts/CaseControlROH/EnsemblGenes_CanFam3.1_geneNames.txt", stringsAsFactors = F)
-bim = read.table("~/DogProject_Jaz/LocalRscripts/PCA_Unrelateds/MergedFile_CornellCanineFitak_allIndivs.bim", stringsAsFactors = F)
+genes = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/EnsemblGenes_CanFam3.1.bed", stringsAsFactors = F)
+gene_names = read.table("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/EnsemblGenes_CanFam3.1_geneNames.txt", stringsAsFactors = F)
+bim = read.table("~/Documents/DogProject_Jaz/LocalRscripts/PCA_Unrelateds/MergedFile_CornellCanineFitak.bim", stringsAsFactors = F)
+nonROH_vcfTools = read.table("~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/CountExonRegion_NonOverlapsROH_vcfTools.bed", stringsAsFactors = F, header = T)
+nonROH_plink = read.table("~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/CountExonRegion_NonOverlapsROH.bed", stringsAsFactors = F, header = T)
 
 ####Make file with genes of interest
 ####Filter to only chromosomes 1-38 
@@ -51,8 +52,49 @@ my_subject = subjectHits(SNP_Gene_Overlaps)
 
 snpDensityPerGene = data.frame(GeneName = geneRanges[my_subject]$id, startPos = geneRanges[my_subject]$startPos, snpStart = posRanges[my_query]$startPos) 
 
-x = merge.data.frame(newGeneSet, snpDensityPerGene, by = c("GeneName", "startPos"), all = T) %>%
-  mutate(snpPresent = ifelse(is.na(snpStart), "0","1")) %>%
-  group_by(GeneName, startPos) %>%
-  mutate(countSNPs = sum(is.numeric(snpPresent))) %>%
-  ungroup()
+GeneSplits = merge.data.frame(newGeneSet, snpDensityPerGene, by = c("GeneName", "startPos"), all = T) %>%
+  group_by(GeneName) %>%
+  count() %>%
+  ungroup() %>%
+  mutate(lengthGene = n*1000)
+
+SNPsPerGene = merge.data.frame(newGeneSet, snpDensityPerGene, by = c("GeneName", "startPos"), all = T) %>%
+  mutate(snpPresent = ifelse(is.na(snpStart), as.numeric(0),as.numeric(1))) %>%
+  group_by(GeneName) %>%
+  summarise_at(c("snpPresent"), sum, na.rm = TRUE) %>% 
+  mutate(lengthGene = GeneSplits$lengthGene[match(GeneName, GeneSplits$GeneName)],
+         propSNPs = snpPresent/lengthGene, 
+         bin = cut(lengthGene, breaks=c(quantile(lengthGene, probs = seq(0, 1, by = 0.20))),labels=c("0-20","20-40","40-60","60-80","80-100"), include.lowest=TRUE),
+         plink = ifelse(GeneName%in%nonROH_plink$GeneName, "1", "0"),
+         vcftools = ifelse(GeneName%in%nonROH_vcfTools$GeneName, "1", "0")) 
+
+#Plots
+ggplot(SNPsPerGene, aes(x=lengthGene, y=snpPresent)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") +
+  theme_bw() + 
+  theme(axis.text.x = element_blank()) 
+
+ggplot(SNPsPerGene, aes(x=bin, y=snpPresent)) + 
+  geom_violin() +
+  geom_point(color=ifelse(SNPsPerGene$plink == "1", "red", "black")) +
+  theme_bw() + 
+  theme(axis.text.x = element_text(size  = 20), 
+        axis.text.y = element_text(size  = 20), 
+        plot.title=element_text(size=26, face = "bold"), 
+        axis.title=element_text(size=24), 
+        legend.title=element_text(size=20), 
+        legend.text=element_text(size=20)) +
+  labs(x= "Quartile Bin", y= "Number of SNPs in Gene") 
+
+
+ggplot(SNPsPerGene, aes(x=bin, y=propSNPs)) + 
+  geom_point() +
+  theme_bw() + 
+  theme(axis.text.x = element_text(size  = 20), 
+        axis.text.y = element_text(size  = 20), 
+        plot.title=element_text(size=26, face = "bold"), 
+        axis.title=element_text(size=24), 
+        legend.title=element_text(size=20), 
+        legend.text=element_text(size=20)) +
+  labs(x= "Quartile Bin", y= "Prop of SNPs in Gene") 
