bf893fa7c2441e51f0a5c3d3166c4a6df9d12eb3
jazlyn mooney <jazlyn.mooney@gmail.com>
2019-09-25 18:04:37 -0700

    updated file extensions

--- InbreedingDepression/vcftools/intersectROHExons.sh
+++ InbreedingDepression/vcftools/intersectROHExons.sh
@@ -9,7 +9,7 @@ module load bedtools
 bedtools sort -i allROH_withIndivCol_vcfTools.bed > SortedROH_withIndivCol_vcfTools.bed
 
 #intersects exon coordinate with ROHs and outputs the exonic region where there are no ROH overlap
-bedtools intersect -a ForAbi_EnsemblGenes_CanFam3.1_SingleTranscript.bed -b SortedROH_withIndivCol_vcfTools.bed -v > ExonRegion_NonOverlapsROH_vcfTools.bed
+bedtools intersect -a ../ForAbi_EnsemblGenes_CanFam3.1_SingleTranscript.bed -b SortedROH_withIndivCol_vcfTools.bed -v > ExonRegion_NonOverlapsROH_vcfTools.bed
 
 #count how many exons per gene have no ROH
 awk '{print $4}' ExonRegion_NonOverlapsROH_vcfTools.bed | uniq -c | sed -e 1i'CountExon\tGeneNames' > CountExonRegion_NonOverlapsROH_vcfTools.bed
