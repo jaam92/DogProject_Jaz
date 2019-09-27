ce9df0bfd6216fdfabb0bfa55898d2989faacc58
jazlyn mooney <jazlyn.mooney@gmail.com>
2019-09-26 19:06:42 -0700

    uncomment update paths

--- InbreedingDepression/HaywardData/intersectROHExons_hayward.sh
+++ InbreedingDepression/HaywardData/intersectROHExons_hayward.sh
@@ -14,25 +14,27 @@ module load R/3.4.2
 module load bedtools
 
 #make vcf
-#plink --dog --bfile /u/home/j/jmooney3/klohmueldata/jazlyn_data/DogProject_Jaz/ogFiles_Hayward2016/cornell_canine --recode vcf-fid --out cornell_canine
+plink --dog --bfile /u/home/j/jmooney3/klohmueldata/jazlyn_data/DogProject_Jaz/ogFiles_Hayward2016/cornell_canine --recode vcf-fid --out cornell_canine
 
 #for f in {1..38} 
-#do 
+do 
+
 #call roh
-#vcftools --vcf cornell_canine.vcf --LROH --chr "$f" --out cornell_canine_chr"$f"
-#done
+vcftools --vcf cornell_canine.vcf --LROH --chr "$f" --out cornell_canine_chr"$f"
+
+done
 
 #Filter ROH
-#cat cornell_canine_chr*.LROH | awk '$6 > 49 {print $0}'| grep -v "CHROM" | sed 1i'CHROM\tAUTO_START\tAUTO_END\tMIN_START\tMAX_END\tN_VARIANTS_BETWEEN_MAX_BOUNDARIES\tN_MISMATCHES\tINDV' > mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.txt
+cat cornell_canine_chr*.LROH | awk '$6 > 49 {print $0}'| grep -v "CHROM" | sed 1i'CHROM\tAUTO_START\tAUTO_END\tMIN_START\tMAX_END\tN_VARIANTS_BETWEEN_MAX_BOUNDARIES\tN_MISMATCHES\tINDV' > mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.txt
 
 #generate true ROH
-#Rscript GenerateTrueROHFiles.R
+Rscript GenerateTrueROHFiles.R
 
 #convert to bed format
 awk '{print "chr"$8"\t"$3-1"\t"$4"\t"$6}' TrueROH_propCoveredwithin1SDMean_allChroms_mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.txt | grep -v "CHROM" > TrueROH_propCoveredwithin1SDMean_allChroms_mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.bed
 
 #sorted all the ROHs by the chromosome
-bedtools sort -i TrueROH_propCoveredwithin1SDMean_allChroms_mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.bed | bedtools intersect -a ForAbi_EnsemblGenes_CanFam3.1_SingleTranscript.bed -b stdin -v > ExonRegion_NonOverlapsROH_cornellData.bed
+bedtools sort -i TrueROH_propCoveredwithin1SDMean_allChroms_mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps.bed | bedtools intersect -a ../ForAbi_EnsemblGenes_CanFam3.1_SingleTranscript.bed -b stdin -v > ExonRegion_NonOverlapsROH_cornellData.bed
 
 #count how many exons per gene have no ROH
 awk '{print $4}' ExonRegion_NonOverlapsROH_cornellData.bed | uniq -c | sed -e 1i'CountExon\tGeneNames' > CountExonRegion_NonOverlapsROH_cornellData.bed
