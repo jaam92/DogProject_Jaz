e43d02e94f602ed3da68d8b741900044992be6da
jaam92 <jazlyn.mooney@gmail.com>
2019-10-16 16:51:12 -0700

    added all traits

--- LocalRscripts/CaseControlROH/AssociationTest.R
+++ LocalRscripts/CaseControlROH/AssociationTest.R
@@ -1,5 +1,6 @@
 #Load libraries
 library(tidyverse)
+library(GMMAT)
 
 #Define fxns
 #Function to pull phenotype data
@@ -21,6 +22,7 @@ qn <- function(exp_vector) {
   result = qnorm(rank(exp_vector)/(length(exp_vector)+1))
   return(result)
 }
+
 #function to run the association
 runAssociation = function(phenoKinshipMatrix, phenotypeColName){
   ROHLoad = colnames(phenoKinshipMatrix) %>% 
@@ -30,14 +32,8 @@ runAssociation = function(phenoKinshipMatrix, phenotypeColName){
            QNSCORE = qn(as.numeric(ROHLoad)), 
            pheno = phenotypes[,phenotypeColName][match(dogID, phenotypes$dogID)],
            status = ifelse(pheno == "2", as.numeric(1), as.numeric(0)))
-  #test whether there are more controls than cases, if not balance the data
-  if(table(ROHLoad$pheno)[1] > table(ROHLoad$pheno)[2]){
-    finalROHLoad = ROHLoad
-  }else{
-    ###start here tomorrow
-    finalROHLoad = 
-  }
-  out.logistic<-glmmkin(status~QNSCORE,data=finalROHLoad,kins=phenoKinshipMatrix*2,id = "dogID",family=binomial(link="logit"))
+  
+  out.logistic<-glmmkin(status~QNSCORE,data=ROHLoad,kins=phenoKinshipMatrix*2,id = "dogID",family=binomial(link="logit"))
   
   LogisticCoef = out.logistic$coef[2]
   LogisticStat = out.logistic$coef[2]/sqrt(out.logistic$cov[2,2])
@@ -47,27 +43,27 @@ runAssociation = function(phenoKinshipMatrix, phenotypeColName){
 }
 
 #Load kinship matrix, ROHs, and phenotype data
-pcRelateMat <- readRDS("~/DogProject_Jaz/LocalRscripts/CaseControlROH/pcRelateMatrix_Unrelateds.rds")
-rohs = read.delim(file = "~/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt") %>%
+pcRelateMat = readRDS("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/pcRelateMatrix_Unrelateds.rds")
+rohs = read.delim(file = "~/Documents/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt") %>%
   group_by(INDV) %>%
   summarise(totalROH = sum(as.numeric(AUTO_LEN)))
-phenotypes = read.delim("~/DogProject_Jaz/LocalRscripts/BreedCladeInfo/phenotypes.txt")  %>% 
+phenotypes = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/BreedCladeInfo/phenotypes.txt")  %>% 
   mutate(PSVA = ifelse(is.na(PSVA), PSVA_yorkshireTerriers, PSVA), 
          MCT = ifelse(is.na(MCT), MCT_labradorRetrievers, MCT), 
          lymphoma = ifelse(is.na(lymphoma), lymphoma_goldenRetrievers, lymphoma))
 
 #Make data frames for each phenotype of interest and run association test
-#Hip dysplasia
-CHD_allBreeds = phenoKinship("CHD")
-
 #Elbow Dysplasia
 ED_allBreeds = phenoKinship("ED")
+runAssociation(ED_allBreeds, "ED")
 
 #Collagen Disorder
 CLLD_allBreeds = phenoKinship("CLLD")
+runAssociation(CLLD_allBreeds, "CLLD")
 
 #Epilepsy Irish Wolfhounds
 IrishWolfhounds = phenoKinship("epilepsy_irishWolfhounds")
+runAssociation(IrishWolfhounds, "epilepsy_irishWolfhounds")
 
 #Lymphoma Golden Retrievers 
 GoldenRetrievers = phenoKinship("lymphoma_goldenRetrievers")
@@ -83,20 +79,26 @@ Boxers = phenoKinship("GC_boxers_bulldogs")
 
 #Crohn's in Boxer and Bulldog
 GC_allBreeds = phenoKinship("GC_boxers_bulldogs")
+runAssociation(GC_allBreeds, "GC_boxers_bulldogs")
 
 #Mast Cell Tumor in Labrador Retrievers 
 #sample equal number of cases and controls
 LabradorRetrievers = phenoKinship("MCT_labradorRetrievers") 
+runAssociation(LabradorRetrievers, "MCT_labradorRetrievers")
 
 #Mast Cell Tumor all Breeds 
 MCT_allBreeds = phenoKinship("MCT") 
+runAssociation(MCT_allBreeds, "MCT")
 
 #PSVA in Yorkshire Terriers
 YorkshireTerriers = phenoKinship("PSVA_yorkshireTerriers")
+runAssociation(YorkshireTerriers, "PSVA_yorkshireTerriers")
 
 #PSVA in all breeds
 #Add some more from the controls in Yorkshire Terrier data
 PSVA_allBreeds = phenoKinship("PSVA")
+runAssociation(PSVA_allBreeds, "PSVA")
 
 #Mitral Valve data
 MitralValve = phenoKinship("MVD") 
+runAssociation(MitralValve, "MVD")
