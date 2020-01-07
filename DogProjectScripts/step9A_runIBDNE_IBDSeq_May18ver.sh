83740117710b4000f19df932524cd566e565e113
jazlyn mooney <jazlyn.mooney@gmail.com>
2020-01-07 09:09:06 -0800

    added more populations

--- DogProjectScripts/step9A_runIBDNE_IBDSeq_May18ver.sh
+++ DogProjectScripts/step9A_runIBDNE_IBDSeq_May18ver.sh
@@ -2,7 +2,7 @@
 #$ -cwd 
 #$ -V 
 #$ -N M18_IBDSeqIBDNe_allpops
-#$ -l highp,time=50:00:00,h_data=10G 
+#$ -l highp,time=70:00:00,h_data=10G 
 #$ -pe shared 2
 #$ -M eplau 
 #$ -m bea
@@ -12,13 +12,13 @@
 . /u/local/Modules/default/init/modules.sh
 module load java
 
-for i in {boxer,cocker_spaniel,german_shepherd_dog,golden_retriever,grayWolf,labrador_retriever,maltese,mix,newfoundland,poodle,rottweiler,village_dog_peru,yorkshire_terrier}
+for i in {boxer,cocker_spaniel,german_shepherd_dog,golden_retriever,labrador_retriever,maltese,mix,newfoundland,poodle,rottweiler,village_dog_peru,yorkshire_terrier,grayWolf_Europe,grayWolf_NorthAmerica}
 
 do
 
 #Run IBDNe of IBDSeq segments (set the minibd=4cm) This is suggested in the IBDNE paper when using IBDSeq on array based data
-#cat IBDSeqIBDNe/"$i"_IBDSeqIBDSegs_allchroms_unrelatedsOnly.ibd | java -jar /u/home/j/jmooney3/klohmueldata/jazlyn_data/software/Phasing_IBD/IBDNe/ibdne.04Sep15.e78.jar map=/u/home/j/jmooney3/klohmueldata/jazlyn_data/DogProject_Jaz/dog_genetic_maps/canFam3.1_average_allChroms_IBDNe.map minibd=4 out=IBDSeqIBDNe/"$i"_IBDNE_usingIBDSeqIBDSegs_Sept15ver
+#cat IBDSeqIBDNe/"$i"_IBDSeqIBDSegs_allchroms_unrelatedsOnly.ibd | java -jar /u/home/j/jmooney3/klohmueldata/jazlyn_data/software/Phasing_IBD/IBDNe/ibdne.04Sep15.e78.jar map=../dog_genetic_maps/canFam3.1_average_allChroms_IBDNe.map minibd=4 out=IBDSeqIBDNe/"$i"_IBDNE_usingIBDSeqIBDSegs_Sept15ver
 
-cat IBDSeqIBDNe/"$i"_IBDSeqIBDSegs_allchroms_unrelatedsOnly.ibd | java -jar /u/home/j/jmooney3/klohmueldata/jazlyn_data/software/Phasing_IBD/IBDNe/ibdne.07May18.6a4.jar map=/u/home/j/jmooney3/klohmueldata/jazlyn_data/DogProject_Jaz/dog_genetic_maps/canFam3.1_average_allChroms_IBDNe.map mincm=4 out=IBDSeqIBDNe/"$i"_IBDNE_usingIBDSeqIBDSegs_May18ver
+cat IBDSeqIBDNe/"$i"_IBDSeqIBDSegs_allchroms_unrelatedsOnly.ibd | java -jar /u/home/j/jmooney3/klohmueldata/jazlyn_data/software/Phasing_IBD/IBDNe/ibdne.07May18.6a4.jar map=../dog_genetic_maps/canFam3.1_average_allChroms_IBDNe.map mincm=4 out=IBDSeqIBDNe/"$i"_IBDNE_usingIBDSeqIBDSegs_May18ver
 
 done
