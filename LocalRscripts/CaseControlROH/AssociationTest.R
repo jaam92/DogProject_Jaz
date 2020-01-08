e42947507cb61e4c8c03e4790766e4fc750fd274
jaam92 <jazlyn.mooney@gmail.com>
2020-01-08 10:19:07 -0800

    loop through all files and run test

--- LocalRscripts/CaseControlROH/AssociationTest.R
+++ LocalRscripts/CaseControlROH/AssociationTest.R
@@ -2,101 +2,63 @@
 library(tidyverse)
 library(GMMAT)
 
-#Define fxns
-
 #Function to inverse normal transform the score
 qn = function(exp_vector) {
   result = qnorm(rank(exp_vector)/(length(exp_vector)+1))
   return(result)
 }
 
-#Load kinship matrix, ROHs, and phenotype data
+#Load kinship matrix, ROHs, and phenotype data filename
 pcRelateMat = readRDS("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/pcRelateMatrix_Unrelateds.rds")
 
 rohs = read.delim(file = "~/Documents/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt") %>%
   group_by(INDV) %>%
   summarise(totalROH = sum(as.numeric(AUTO_LEN))) 
 
-fnames = list.files(path="~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile", pattern = "\\.txt$")
+fnames = paste0("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/", list.files(path="~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile", pattern = "\\.txt$")) #paste the path in front of the filename
 
-#Generate data frame
-##create columns with fileName, population, and compute pi 
-df = rbindlist(sapply(fnames, read.delim, simplify = FALSE), use.names = TRUE, idcol = "FileName")
+#Loop through all files and run association test
+AssociationTestResults = data.frame() #make data frame to store results
+for (i in seq_along(fnames)){
+  phenotypes = read.delim(fnames[i]) #read file in
+  names(phenotypes)[3] = "trait" #rename third column to trait
+  trait = gsub(".*[/]([^.]+)[.txt].*", "\\1", fnames[i]) #keep track of trait and breed tested
+  
+  #keep only those rows and columns that correspond to dogs of interest
+  kinshipMat = pcRelateMat %>%
+    as.matrix()
+  kinshipMat = kinshipMat[, colnames(kinshipMat) %in% phenotypes$dogID]
+  kinshipMat = kinshipMat[ rownames(kinshipMat) %in% phenotypes$dogID,]
+  
+  #Grab the ROH information
+  ROHLoad = colnames(kinshipMat) %>% 
+    as.data.frame() %>% 
+    rename("dogID" = ".") %>%
+    mutate(ROHLoad = rohs$totalROH[match(dogID, rohs$INDV)],
+           QNSCORE = qn(as.numeric(ROHLoad)), 
+           pheno = phenotypes$trait[match(dogID, phenotypes$dogID)],
+           status = gsub("1", "0", pheno), 
+           status = gsub("2", "1", status),
+           status = as.numeric(status)) #recode case-control status to 0 and 1
+  
+  #Count number of cases and controls
+  CaseControlCount = ROHLoad %>% 
+    count(status) %>% 
+    pivot_wider(names_from = status, values_from = n) %>% 
+    rename("Case" = "1", "Control" = "0")
+  
+  #Run the glmm as a logisitic, compute coeffcients and  p-value  
+  out.logistic = glmmkin(status~QNSCORE, data=ROHLoad, kins=kinshipMat*2, id = "dogID", family=binomial(link="logit"))
+  LogisticCoef = out.logistic$coef[2]
+  LogisticStat = out.logistic$coef[2]/sqrt(out.logistic$cov[2,2])
+  LogisticPval = ifelse(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2])<0, pnorm(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2]),lower=TRUE)*2,pnorm(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2]),lower=FALSE)*2)
+  
+  #Save Association Test output case-control count
+  LogRegOutput = cbind.data.frame(trait, CaseControlCount, LogisticCoef,LogisticStat,LogisticPval)
+  AssociationTestResults = rbind.data.frame(AssociationTestResults, LogRegOutput)
+}
 
-phenotypes = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/CLLD_german_shepherd_dog.txt")
-
-names(phenotypes)[3] = "trait"
-#keep only those rows and columns that correspond to dogs of interest
-kinshipMat = pcRelateMat %>%
-  as.matrix()
-kinshipMat = kinshipMat[, colnames(kinshipMat) %in% phenotypes$dogID]
-kinshipMat = kinshipMat[ rownames(kinshipMat) %in% phenotypes$dogID,]
-
-#Grab the ROH information
-ROHLoad = colnames(kinshipMat) %>% 
-  as.data.frame() %>% 
-  rename("dogID" = ".") %>%
-  mutate(ROHLoad = rohs$totalROH[match(dogID, rohs$INDV)],
-         QNSCORE = qn(as.numeric(ROHLoad)), 
-         pheno = phenotypes$trait[match(dogID, phenotypes$dogID)],
-         status = ifelse(pheno == "2", as.numeric(1), as.numeric(0)))
-
-out.logistic = glmmkin(status~QNSCORE, data=ROHLoad, kins=kinshipMat*2, id = "dogID", family=binomial(link="logit"))
-
-LogisticCoef = out.logistic$coef[2]
-LogisticStat = out.logistic$coef[2]/sqrt(out.logistic$cov[2,2])
-LogisticPval = ifelse(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2])<0, pnorm(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2]),lower=TRUE)*2,pnorm(out.logistic$coef[2]/sqrt(out.logistic$cov[2,2]),lower=FALSE)*2)
-LogRegOutput = cbind.data.frame(LogisticCoef,LogisticStat,LogisticPval,fname[i])
 
 
 
-#Make data frames for each phenotype of interest and run association test
-#Elbow Dysplasia
-ED_allBreeds = phenoKinship("ED")
-runAssociation(ED_allBreeds, "ED")
 
-#Collagen Disorder
-CLLD_allBreeds = phenoKinship("CLLD")
-runAssociation(CLLD_allBreeds, "CLLD")
-
-#Epilepsy Irish Wolfhounds
-IrishWolfhounds = phenoKinship("epilepsy_irishWolfhounds")
-runAssociation(IrishWolfhounds, "epilepsy_irishWolfhounds")
-
-#Lymphoma Golden Retrievers 
-GoldenRetrievers = phenoKinship("lymphoma_goldenRetrievers")
-runAssociation(GoldenRetrievers, "lymphoma_goldenRetrievers")
-
-#Lymphoma all Breeds
-#sample equal number of cases and controls
-Lymphoma_allBreeds = phenoKinship("lymphoma")
-runAssociation(Lymphoma_allBreeds, "lymphoma")
-
-#Crohn's in Boxer
-Boxers = phenoKinship("GC_boxers_bulldogs")
-
-#Crohn's in Boxer and Bulldog
-GC_allBreeds = phenoKinship("GC_boxers_bulldogs")
-runAssociation(GC_allBreeds, "GC_boxers_bulldogs")
-
-#Mast Cell Tumor in Labrador Retrievers 
-#sample equal number of cases and controls
-LabradorRetrievers = phenoKinship("MCT_labradorRetrievers") 
-runAssociation(LabradorRetrievers, "MCT_labradorRetrievers")
-
-#Mast Cell Tumor all Breeds 
-MCT_allBreeds = phenoKinship("MCT") 
-runAssociation(MCT_allBreeds, "MCT")
-
-#PSVA in Yorkshire Terriers
-YorkshireTerriers = phenoKinship("PSVA_yorkshireTerriers")
-runAssociation(YorkshireTerriers, "PSVA_yorkshireTerriers")
-
-#PSVA in all breeds
-#Add some more from the controls in Yorkshire Terrier data
-PSVA_allBreeds = phenoKinship("PSVA")
-runAssociation(PSVA_allBreeds, "PSVA")
-
-#Mitral Valve data
-MitralValve = phenoKinship("MVD") 
-runAssociation(MitralValve, "MVD")
