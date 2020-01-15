2f21b22c1777567bbe250b644e4bec25c9eebec2
jaam92 <jazlyn.mooney@gmail.com>
2020-01-15 10:44:20 -0800

    added code that cleans data with all samples for trait

--- LocalRscripts/CaseControlROH/cleanUpPhenotypeNSplit.R
+++ LocalRscripts/CaseControlROH/cleanUpPhenotypeNSplit.R
@@ -1,7 +1,56 @@
 #Libraries
 library(tidyverse)
 
-#Function to make a list of data frames for a given phenotype of interest
+#Functions to make a list of data frames for a given phenotype of interest for each breed and over all breeds
+
+#all Breeds fxn
+balancePheno = function(phenoColName){
+  df = as.data.frame() #make an empty list to hold my list of data frames
+  phenoCol = enquo(phenoColName) #grab the phenotype column
+  
+  #Make a data frame with breed num case controls and indicator col for sampling
+  #This fxn will identify whether there may be more cases than controls for each breed (list is per breed)
+  #If there are more cases than controls downsample and output an equal number than match the sample size of controls 
+  #Only output data if there are at least 10 cases and controls
+  df = phenotypes %>% 
+    select(dogID, breed, !!phenoCol) %>%
+    filter(dogID %in% indivs$dogID & breed != "mix") %>% #remove mixed breed dogs
+    na.omit() %>%
+    dplyr::rename(trait = !!phenoCol) %>% #rename phenotype col makes things easier when pivotting dataframe 
+    group_by(trait) %>%
+    count() %>%
+    pivot_wider(names_from = trait, values_from = n ) %>%
+    dplyr::rename(control=`1`, case=`2`) %>%
+    mutate(downSamp=case_when(
+      control > case ~ as.integer(0), 
+      control == case ~ as.integer(0),
+      case > control ~ as.integer(control))) %>%
+    filter(control >= 10 & case >= 10)
+  
+  #Write each component of list to separate file and downsample if needed
+    if(df$downSamp != 0){
+      finalDF = phenotypes %>%
+        select(dogID, breed, !!phenoCol) %>% 
+        dplyr::rename(trait = !!phenoCol) %>% #rename column to get group_by to work
+        filter(dogID %in% indivs$dogID & breed != "mix") %>% #use only dogs that pass QC and aren't mixed breed
+        na.omit() %>%
+        group_by(trait) %>%
+        sample_n(size = dfList$downSamp) #downsample cases to match controls
+      names(finalDF)[3] = phenoColName #put original column name back
+    }else{
+      finalDF = phenotypes %>%
+        select(dogID, breed, !!phenoCol) %>% 
+        filter(dogID %in% indivs$dogID & breed != "mix") %>% #use only dogs that pass QC and aren't mixed breed
+        na.omit()
+    }
+    
+    #write the final data frame to file
+    write.table(finalDF, file=paste0("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/", phenoColName, ".txt"), row.names = FALSE, col.names = TRUE, quote = FALSE, sep="\t")
+    
+  }
+  
+
+#per breed fxn
 balancePhenoPerBreed = function(phenoColName){
   dfList = list() #make an empty list to hold my list of data frames
   phenoCol = enquo(phenoColName) #grab the phenotype column
@@ -65,7 +114,7 @@ balancePhenoPerBreed = function(phenoColName){
   return(dfList)
 }
 
-#Load file
+#Load files
 popmapDryad = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/BreedCladeInfo/breeds_dryad.txt")
 phenotypes = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/BreedCladeInfo/phenotypes.txt")  %>% 
     mutate(PSVA = ifelse(is.na(PSVA), PSVA_yorkshireTerriers, PSVA), 
@@ -77,25 +126,33 @@ indivs = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/BreedCladeInfo/Ind
 #Make data frames for each phenotype of interest and output new phenotype files
 set.seed(303)
 #Elbow Dysplasia
-ED_allBreeds = balancePhenoPerBreed("ED")
+ED_allBreeds = balancePheno("ED")
+ED_perBreed = balancePhenoPerBreed("ED")
 
 #Collagen Disorder
-CLLD_allBreeds = balancePhenoPerBreed("CLLD")
+CLLD_allBreeds = balancePheno("CLLD")
+CLLD_perBreed = balancePhenoPerBreed("CLLD")
 
 #Epilepsy Irish Wolfhounds
-IrishWolfhounds_allBreeds = balancePhenoPerBreed("epilepsy_irishWolfhounds")
+IrishWolfhounds_allBreeds = balancePheno("epilepsy_irishWolfhounds")
+IrishWolfhounds_perBreed = balancePhenoPerBreed("epilepsy_irishWolfhounds")
 
 #Lymphoma all Breeds
-Lymphoma_allBreeds = balancePhenoPerBreed("lymphoma")
+Lymphoma_allBreeds = balancePheno("lymphoma")
+Lymphoma_perBreed = balancePhenoPerBreed("lymphoma")
 
 #Crohn's in Boxer and Bulldog
-GC_allBreeds = balancePhenoPerBreed("GC_boxers_bulldogs")
+GC_allBreeds = balancePheno("GC_boxers_bulldogs")
+GC_perBreed = balancePhenoPerBreed("GC_boxers_bulldogs")
 
 #Mast Cell Tumor all Breeds 
-MCT_allBreeds = balancePhenoPerBreed("MCT") 
+MCT_allBreeds = balancePheno("MCT") 
+MCT_perBreed = balancePhenoPerBreed("MCT") 
 
 #PSVA in all breeds
-PSVA_allBreeds = balancePhenoPerBreed("PSVA")
+PSVA_allBreeds = balancePheno("PSVA")
+PSVA_perBreed = balancePhenoPerBreed("PSVA")
 
 #Mitral Valve data
-MitralValve_allBreeds = balancePhenoPerBreed("MVD") 
+MitralValve_allBreeds = balancePheno("MVD") 
+MitralValve_perBreed = balancePhenoPerBreed("MVD") 
