1acee051f2cb1d671c559471f7d0790ead5dc9d1
jaam92 <jazlyn.mooney@gmail.com>
2020-01-31 15:25:02 -0800

    new script to examine how ROH overlap 100kb genomic windows

---
+++ LocalRscripts/InbreedingDepAnalyses/ExamineIndivOverlaps_ROHAutosomalWindows.R
@@ -0,0 +1,36 @@
+#Load libraries
+library(tidyverse)
+
+#Read empirical data in
+df = read.table("~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/CountEmpiricalOverlaps_100Kb_AutosomalSplits.bed") %>%
+  mutate(chrom = as.numeric(gsub("chr", "", V2)),
+         proportion = V1/4342) %>%
+  arrange(chrom) 
+
+avgPerChrom = df %>%
+  group_by(chrom) %>%
+  summarise(mean = mean(proportion)) 
+
+#Look at the distribution of empirical data overlaps
+ggplot(df, aes(x=V1)) +
+  geom_density() +
+  theme_bw()
+
+ggplot(data = df, aes(x=V3,y=proportion)) +
+  geom_point() +
+  geom_hline(aes(yintercept=mean(df$proportion), linetype = "Genome-wide average"), colour= 'red') +
+  geom_hline(data = avgPerChrom, aes(yintercept=mean, linetype="Chromosome average"), colour= 'blue') +
+  facet_wrap(~chrom, scales = "free") +
+  scale_linetype_manual(name = "Region", values = c(1, 2), 
+                        guide = guide_legend(override.aes = list(color = c("blue", "red")))) +
+  scale_y_continuous(labels=scales::percent) +
+  
+  labs(x="Position along chromosome (base pairs)", y="Number of overlapping ROHs") +
+  theme_bw()  + 
+  theme(axis.text.x = element_text(hjust= 0.8, vjust=0.8, angle = 45, size=14), 
+        axis.text.y = element_text(size = 16), 
+        plot.title=element_text(size = 20, face = "bold", hjust=0.5), 
+        axis.title=element_text(size= 18),
+        strip.text = element_text(size= 14),
+        legend.title=element_text(size= 18), 
+        legend.text=element_text(size= 16)) 
