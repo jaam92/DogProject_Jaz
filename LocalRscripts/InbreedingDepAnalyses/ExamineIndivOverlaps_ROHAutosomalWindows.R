970f8597812c17c2495829bf0b5622b133ddb294
jaam92 <jazlyn.mooney@gmail.com>
2020-01-31 16:07:49 -0800

    updated plots

--- LocalRscripts/InbreedingDepAnalyses/ExamineIndivOverlaps_ROHAutosomalWindows.R
+++ LocalRscripts/InbreedingDepAnalyses/ExamineIndivOverlaps_ROHAutosomalWindows.R
@@ -3,8 +3,8 @@ library(tidyverse)
 
 #Read empirical data in
 df = read.table("~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/CountEmpiricalOverlaps_100Kb_AutosomalSplits.bed") %>%
-  mutate(chrom = as.numeric(gsub("chr", "", V2)),
-         proportion = V1/4342) %>%
+  mutate(chrom = as.numeric(gsub("chr", "", V1)),
+         proportion = V4/4342) %>%
   arrange(chrom) 
 
 avgPerChrom = df %>%
@@ -12,10 +12,36 @@ avgPerChrom = df %>%
   summarise(mean = mean(proportion)) 
 
 #Look at the distribution of empirical data overlaps
-ggplot(df, aes(x=V1)) +
+ggplot(df, aes(x=V4)) +
   geom_density() +
-  theme_bw()
+  labs(x="Number of ROH overlaps\nper 100Kb segment") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(size=18), 
+        axis.text.y = element_text(size =18), 
+        plot.title=element_text(size = 22, face = "bold", hjust=0.5), 
+        axis.title=element_text(size= 20)) 
 
+#Everything on same scale
+ggplot(data = df, aes(x=V3,y=proportion)) +
+  geom_point() +
+  geom_hline(aes(yintercept=mean(df$proportion), linetype = "Genome-wide average"), colour= 'red') +
+  geom_hline(data = avgPerChrom, aes(yintercept=mean, linetype="Chromosome average"), colour= 'blue') +
+  facet_wrap(~chrom) +
+  scale_linetype_manual(name = "Region", values = c(1, 2), 
+                        guide = guide_legend(override.aes = list(color = c("blue", "red")))) +
+  scale_y_continuous(labels=scales::percent) +
+  
+  labs(x="Position along chromosome (base pairs)", y="Number of overlapping ROHs") +
+  theme_bw()  + 
+  theme(axis.text.x = element_text(hjust= 0.8, vjust=0.8, angle = 45, size=14), 
+        axis.text.y = element_text(size = 16), 
+        plot.title=element_text(size = 20, face = "bold", hjust=0.5), 
+        axis.title=element_text(size= 18),
+        strip.text = element_text(size= 14),
+        legend.title=element_text(size= 18), 
+        legend.text=element_text(size= 16)) 
+
+#Everything on different scale
 ggplot(data = df, aes(x=V3,y=proportion)) +
   geom_point() +
   geom_hline(aes(yintercept=mean(df$proportion), linetype = "Genome-wide average"), colour= 'red') +
