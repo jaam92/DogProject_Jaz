d366f6734f66877b2e90277c42595f61c0229ca3
jaam92 <jazlyn.mooney@gmail.com>
2020-02-21 12:04:41 -0800

    rohs across all individuals

--- LocalRscripts/InbreedingDepAnalyses/shuffROH_perms.R
+++ LocalRscripts/InbreedingDepAnalyses/shuffROH_perms.R
@@ -7,10 +7,13 @@ newCoords = data.frame() #keep first set of rohs
 updatedROHs = data.frame() #keep rohs when overlaps are detected
 finalROH = data.frame() #permuted rohs
 rohsMappedBack = data.frame() #permuted rohs that are mapped back to original chroms
+everyonesROHs = list() #keep everyones new rohs
 
-#Load breed info, ROHs, and kinship matrix
-rohs = read.delim(file = "~/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt", stringsAsFactors = F) %>% 
-  filter(INDV == "30") 
+#Load dog info and ROHs
+dogInfo = read.delim("~/DogProject_Jaz/LocalRscripts/BreedCladeInfo/breeds_dryad.txt")
+rohs = read.delim(file = "~/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt", stringsAsFactors = F) %>%
+  filter(INDV %in% dogInfo$dogID) #subset to Hayward data only
+indivs = unique(rohs$INDV) #grab all individuals
 
 #use the cumulative sum as the end since we are shuffling over the entire length of the genome 
 autosome = read.delim(file = "~/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/chromosomeLengths.txt", check.names = F, stringsAsFactors = F, sep = " ") %>%
@@ -29,73 +32,81 @@ autoLen = sum(as.numeric(autosome$LENGTH)) #compute the length of the autsome si
 
 #Shuffle the ROH
 #Move runs on same chrom by sampling uniformly
-for(i in rohs$AUTO_LEN){
-  rohLength = i
-  start = runif(1, min = 1, max = autoLen)
-  end = start + rohLength
-  #Next check whether rohs fall within a chromosome, if they do not make new roh until it does
-  if (end <= autoLen) {
-    newROH = cbind(start, end)
-    newCoords = rbind(newCoords, newROH)
-  }else{
-    while (end > autoLen) {
-      start = runif(1, min = 1, max = autoLen)
-      end = start + rohLength
-    }
-    newROH = cbind(start, end)
-    newCoords = rbind(newCoords, newROH)
-  }
+for(j in indivs){
   
-  #Check for overlapping rohs if there are overlaps redo everything from the top until there are none
-  finalROH = newCoords %>%
-    mutate(start = start/10^3,
-           end = end/10^3) #scale everything to kb so that integers in IRanges don't overflow
-  finalROH$chrom = "chr1"
-  rohRanges = with(finalROH, GRanges(chrom, IRanges(start=start, end =end)))
-  noOverlaps = rohRanges[unique(findOverlaps(rohRanges, type = "any", select = "first"))] %>%
-    as.data.frame() %>%
-    select(start, end)
+  #subset rohs to contain only individual of interest
+  df = rohs %>%
+    filter(INDV == j)
   
-  if (nrow(finalROH) == nrow(newCoords)) {
-    cat(sprintf("new set rohs created"), sep = "\n")
-    
-  }else{
-    while (nrow(finalROH) < nrow(newCoords)) {
-      cat(sprintf("overlaps detected generating new set of rohs"), sep = "\n")
-      
-      #Re-run everything from the top
-      start = runif(1, min = 1, max = autoLen)
-      end = start + rohLength
-      if (end <= autoLen) {
-        retryROH = cbind(start, end)
-        updatedROHs = rbind(finalROH, retryROH)
-      }else{
-        while (end > autoLen) {
-          start = runif(1, min = 1, max = autoLen)
-          end = start + rohLength
-        }
-        retryROH = cbind(start, end)
-        updatedROHs = rbind(finalROH, retryROH)
-        
+  for(i in df$AUTO_LEN){
+    rohLength = i
+    start = runif(1, min = 1, max = autoLen)
+    end = start + rohLength
+    #Next check whether rohs fall within a chromosome, if they do not make new roh until it does
+    if (end <= autoLen) {
+      newROH = cbind(start, end)
+      newCoords = rbind(newCoords, newROH)
+    }else{
+      while (end > autoLen) {
+        start = runif(1, min = 1, max = autoLen)
+        end = start + rohLength
       }
-      #Check for overlaps again
-      makeNewROHs = updatedROHs %>%
-        mutate(chrom = "chr1",
-               start = start/10^3,
-               end = end/10^3) #scale everything to kb so that integers in IRanges don't overflow
-      rohRanges = with(makeNewROHs, GRanges(chrom, IRanges(start=start, end =end)))
-      finalROH = rohRanges[unique(findOverlaps(rohRanges, type = "any", select = "first"))] %>%
-        as.data.frame() %>%
-        select(start, end)
+      newROH = cbind(start, end)
+      newCoords = rbind(newCoords, newROH)
     }
     
+    #Check for overlapping rohs if there are overlaps redo everything from the top until there are none
+    finalROH = newCoords %>%
+      mutate(start = start/10^3,
+             end = end/10^3) #scale everything to kb so that integers in IRanges don't overflow
+    finalROH$chrom = "chr1"
+    rohRanges = with(finalROH, GRanges(chrom, IRanges(start=start, end =end)))
+    noOverlaps = rohRanges[unique(findOverlaps(rohRanges, type = "any", select = "first"))] %>%
+      as.data.frame() %>%
+      select(start, end)
+    
+    if (nrow(finalROH) == nrow(newCoords)) {
+      cat(sprintf("new set rohs created"), sep = "\n")
+      
+    }else{
+      while (nrow(finalROH) < nrow(newCoords)) {
+        cat(sprintf("overlaps detected generating new set of rohs"), sep = "\n")
+        
+        #Re-run everything from the top
+        start = runif(1, min = 1, max = autoLen)
+        end = start + rohLength
+        if (end <= autoLen) {
+          retryROH = cbind(start, end)
+          updatedROHs = rbind(finalROH, retryROH)
+        }else{
+          while (end > autoLen) {
+            start = runif(1, min = 1, max = autoLen)
+            end = start + rohLength
+          }
+          retryROH = cbind(start, end)
+          updatedROHs = rbind(finalROH, retryROH)
+          
+        }
+        #Check for overlaps again
+        makeNewROHs = updatedROHs %>%
+          mutate(chrom = "chr1",
+                 start = start/10^3,
+                 end = end/10^3) #scale everything to kb so that integers in IRanges don't overflow
+        rohRanges = with(makeNewROHs, GRanges(chrom, IRanges(start=start, end =end)))
+        finalROH = rohRanges[unique(findOverlaps(rohRanges, type = "any", select = "first"))] %>%
+          as.data.frame() %>%
+          select(start, end)
+      }
+      
+    }
+    #map rohs back to chromosomes
+    rangeA = with(finalROH, GRanges(chrom, IRanges(start=start, end =end)))
+    rangeB = with(mapChrom, GRanges(chrom, IRanges(start=start, end =end, names= CHROM)))
+    type2 = findOverlaps(query = rangeA, subject = rangeB, type = 'any')
+    rohsMappedBack = data.frame(finalROH[queryHits(type2),], mapChrom[subjectHits(type2),]) %>%
+      select(CHROM, start, end) %>%
+      mutate(start = start*10^3,
+             end = end*10^3)
   }
-  #map rohs back to chromosomes
-  rangeA = with(finalROH, GRanges(chrom, IRanges(start=start, end =end)))
-  rangeB = with(mapChrom, GRanges(chrom, IRanges(start=start, end =end, names= CHROM)))
-  type2 = findOverlaps(query = rangeA, subject = rangeB, type = 'any')
-  rohsMappedBack = data.frame(finalROH[queryHits(type2),], mapChrom[subjectHits(type2),]) %>%
-    select(CHROM, start, end) %>%
-    mutate(start = start*10^3,
-           end = end*10^3)
+  everyonesROHs[[j]] = list(rohsMappedBack)
 }
