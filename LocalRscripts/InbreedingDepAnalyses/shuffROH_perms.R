387874dc81d66501bfd9e05aaf1e65c42c38f2aa
jaam92 <jazlyn.mooney@gmail.com>
2020-02-24 10:56:48 -0800

    write to file rather than storing as list

--- LocalRscripts/InbreedingDepAnalyses/shuffROH_perms.R
+++ LocalRscripts/InbreedingDepAnalyses/shuffROH_perms.R
@@ -7,16 +7,15 @@ newCoords = data.frame() #keep first set of rohs
 updatedROHs = data.frame() #keep rohs when overlaps are detected
 finalROH = data.frame() #permuted rohs
 rohsMappedBack = data.frame() #permuted rohs that are mapped back to original chroms
-everyonesROHs = list() #keep everyones new rohs
 
 #Load dog info and ROHs
-dogInfo = read.delim("~/DogProject_Jaz/LocalRscripts/BreedCladeInfo/breeds_dryad.txt")
-rohs = read.delim(file = "~/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt", stringsAsFactors = F) %>%
+dogInfo = read.delim("~/Documents/DogProject_Jaz/LocalRscripts/BreedCladeInfo/breeds_dryad.txt")
+rohs = read.delim(file = "~/Documents/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFitakCornell.txt", stringsAsFactors = F) %>%
   filter(INDV %in% dogInfo$dogID) #subset to Hayward data only
 indivs = unique(rohs$INDV) #grab all individuals
 
 #use the cumulative sum as the end since we are shuffling over the entire length of the genome 
-autosome = read.delim(file = "~/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/chromosomeLengths.txt", check.names = F, stringsAsFactors = F, sep = " ") %>%
+autosome = read.delim(file = "~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/chromosomeLengths.txt", check.names = F, stringsAsFactors = F, sep = " ") %>%
   mutate(CHROM = as.numeric(gsub("chr", "", CHROM)),
          endUnif = cumsum(as.numeric(LENGTH)),
          START = as.numeric(endUnif) - as.numeric(LENGTH))
@@ -32,8 +31,8 @@ autoLen = sum(as.numeric(autosome$LENGTH)) #compute the length of the autsome si
 
 #Shuffle the ROH
 #Move runs on same chrom by sampling uniformly
-for(j in indivs){
-  
+for(j in x){
+  print(j)
   #subset rohs to contain only individual of interest
   df = rohs %>%
     filter(INDV == j)
@@ -66,11 +65,11 @@ for(j in indivs){
       select(start, end)
     
     if (nrow(finalROH) == nrow(newCoords)) {
-      cat(sprintf("new set rohs created"), sep = "\n")
+      #cat(sprintf("new set rohs created"), sep = "\n")
       
     }else{
       while (nrow(finalROH) < nrow(newCoords)) {
-        cat(sprintf("overlaps detected generating new set of rohs"), sep = "\n")
+        #cat(sprintf("overlaps detected generating new set of rohs"), sep = "\n")
         
         #Re-run everything from the top
         start = runif(1, min = 1, max = autoLen)
@@ -99,14 +98,16 @@ for(j in indivs){
       }
       
     }
-    #map rohs back to chromosomes
+    #map rohs back to chromosomes and subtract 1 for .bed formatting
     rangeA = with(finalROH, GRanges(chrom, IRanges(start=start, end =end)))
     rangeB = with(mapChrom, GRanges(chrom, IRanges(start=start, end =end, names= CHROM)))
     type2 = findOverlaps(query = rangeA, subject = rangeB, type = 'any')
     rohsMappedBack = data.frame(finalROH[queryHits(type2),], mapChrom[subjectHits(type2),]) %>%
       select(CHROM, start, end) %>%
-      mutate(start = start*10^3,
-             end = end*10^3)
+      mutate(CHROM = paste0("chr", CHROM),
+             start = trunc(as.numeric(start*10^3)-1),
+             end = trunc(end*10^3))
   }
-  everyonesROHs[[j]] = list(rohsMappedBack)
+  write.table(rohsMappedBack, file = "~/Documents/DogProject_Jaz/LocalRscripts/InbreedingDepAnalyses/test.bed", append = T, quote = F, sep = "\t", row.names = F, col.names = F)
+  #everyonesROHs[[j]] = list(rohsMappedBack)
 }
