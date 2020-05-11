e0fcbf0b38d7bb29198d04ae8a2ffcc99cc55350
jazlyn mooney <jazlyn.mooney@gmail.com>
2020-05-11 08:23:00 -0700

    summed wrong column intially, should have summed col 6 not 5

--- ROHGRM/vcftools/step2_pairwiseComps_array.sh
+++ ROHGRM/vcftools/step2_pairwiseComps_array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#$ -l h_data=2G,h_rt=10:00:00
+#$ -l h_data=2G,h_rt=22:00:00
 #$ -cwd
 #$ -A jmooney3
-#$ -N GRMRetry
+#$ -N GRM
 #$ -e /u/flashscratch/j/jmooney3
 #$ -o /u/flashscratch/j/jmooney3
 #$ -m a
@@ -20,7 +20,7 @@ module load xz
 while read -r a b 
 do 
 
-/u/home/j/jmooney3/klohmueldata/jazlyn_data/software/bedtools2/bin/bedtools coverage -a splitFiles/"$a" -b splitFiles/"$b" | awk '{sum +=5} END {print "S=" sum}' | sed -e "s|^|$a\t$b\t|g" >> Output/'overlaps'$SGE_TASK_ID'.bed' 
+/u/home/j/jmooney3/klohmueldata/jazlyn_data/software/bedtools2/bin/bedtools coverage -a splitFiles/"$a" -b splitFiles/"$b" | awk '{sum +=6} END {print "S=" sum}' | sed -e "s|^|$a\t$b\t|g" >> Output/'overlaps'$SGE_TASK_ID'.bed' 
 
 #Let us know where we are
 echo "$a" "$b" 
