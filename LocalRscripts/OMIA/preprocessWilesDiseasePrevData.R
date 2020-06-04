476d7880e37a8f773637876317e3af532f448777
jaam92 <jazlyn.mooney@gmail.com>
2020-06-04 15:50:45 -0700

    updated some breed names

--- LocalRscripts/OMIA/preprocessWilesDiseasePrevData.R
+++ LocalRscripts/OMIA/preprocessWilesDiseasePrevData.R
@@ -3,6 +3,7 @@ setwd("~/Documents/DogProject_Jaz/LocalRscripts/OMIA")
 
 #####Load Libraries
 library(tidyverse)
+library(mgsub)
 
 ####Read input file with prevalence data from Wiles et. al 2017 Canine Evolutionary Genomics
 
@@ -19,8 +20,9 @@ colnames(preprocess) = colnames(preprocess) %>%
   str_replace_all(fixed(" "), "_")
 
 colnames(preprocess)[1] = "trait" #give the first column a name
-colnames(preprocess)[18] = "flatcoated_retriever" #rename the flat-coated retriever
-colnames(preprocess)[30] = "poodle_miniature" #rename the first instance of poodle as mini the second instance is standard
+colnames(preprocess)[30] = "poodle_miniature" #rename the first instance of poodle as mini the second instance is standard need to do this separateley from gsub because poodle occurs twice in data
+colnames(preprocess) = mgsub(colnames(preprocess), pattern =c("flat_coated_retriever", "bulldog", "french_bulldog", "dobermann", "hungarian_vizsla"), replacement =c("flatcoated_retriever", "bulldog_english", "bulldog_french", "doberman_pinscher", "vizsla"))
+
 
 postProcess = preprocess[,-1] %>% 
   map_dfr(~ parse_number(.x,"[[:digit:]]+")) %>%
