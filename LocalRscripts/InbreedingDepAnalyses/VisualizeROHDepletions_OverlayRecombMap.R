a84764f773bc295b2535a218aaf1e9a1c4998923
jaam92 <jazlyn.mooney@gmail.com>
2020-07-08 14:23:38 -0700

    made roh plots across the genome

--- LocalRscripts/InbreedingDepAnalyses/VisualizeROHDepletions_OverlayRecombMap.R
+++ LocalRscripts/InbreedingDepAnalyses/VisualizeROHDepletions_OverlayRecombMap.R
@@ -60,10 +60,11 @@ foldChange_exons = exons %>%
 
 bindDF_foldChange = foldChange %>%
   rbind.data.frame(foldChange_exons) %>%
-  na.omit()
+  na.omit() %>%
+  rename(V1 = "chromo", V2 = "window_start", V3 = "window_end", V4 = "overlaps")
 
-#Everything on different scale
-ggplot(data = bindDF_foldChange, aes(x=V3, y=proportion, colour=data)) +
+#Everything on different scale and split by chromosome
+ggplot(data = bindDF_foldChange, aes(x=window_start, y=proportion, colour=data)) +
   geom_point() +
   facet_wrap(~chrom, scales = "free") +
   scale_color_manual(values = c("VCFTools" = "gray50", "PLINK" = "darkorange", "ANKH" = "blue",  "exon" = "gray50", "FYTTD1" = "steelblue", "no ROH" = "purple", "PRMT2" = "red")) +  
@@ -78,10 +79,10 @@ ggplot(data = bindDF_foldChange, aes(x=V3, y=proportion, colour=data)) +
         legend.text=element_text(size= 16)) #+ 
   #ylim(0,2) 
 
-#Plot with recombination map
+#Plot separated by chromosome with recombination map
 ggplot(data = recomMapAllChroms, aes(x=pos, y=avgPerBin)) +
   geom_line(colour = "orange") +
-  geom_point(data = bindDF_foldChange, aes(x=V3, y=proportion, colour=data)) +
+  geom_point(data = bindDF_foldChange, aes(x=window_start, y=proportion, colour=data)) +
   facet_wrap(~chrom, scales = "free") +
   scale_color_manual(values = c("VCFTools" = "gray50", "PLINK" = "darkorange", "ANKH" = "blue",  "exon" = "steelblue", "FYTTD1" = "gray50", "no ROH" = "purple", "PRMT2" = "red")) +  
   labs(x="Position along chromosome (base pairs)", y="Fold-change overlapping ROHs (relative to chromosomal mean)") +
@@ -94,3 +95,55 @@ ggplot(data = recomMapAllChroms, aes(x=pos, y=avgPerBin)) +
         legend.title=element_text(size= 18), 
         legend.text=element_text(size= 16)) #+ 
   #ylim(0,2)
+
+
+#plot all the chromosomes together like the sliding window plots
+
+#reformat data and rescale windows
+rescaledWindows = bindDF_foldChange %>% 
+  
+  # Compute chromosome size
+  group_by(chrom) %>% 
+  summarise(chr_len=max(window_start)) %>% 
+  
+  # Calculate cumulative position of each chromosome
+  mutate(tot=cumsum(as.numeric(chr_len))-as.numeric(chr_len)) %>%
+  select(-chr_len) %>%
+  
+  # Add this info to the initial dataset
+  left_join(bindDF_foldChange, ., by=c("chrom"="chrom")) %>%
+  
+  # Add a cumulative position of each window
+  arrange(chrom, window_start) %>%
+  mutate( newWinStart=window_start+tot)
+
+#Function to plot data for each individual
+plotFunction = function(dataFrame,dataWanted, color1, color2) {
+  #filter data wanted
+  newDataFrame = dataFrame %>%
+    filter(data == dataWanted)
+  #Generate x axis with any one of the data frames 
+  axisdf = newDataFrame %>% 
+    group_by(chrom) %>% 
+    summarize(center=(max(newWinStart) + min(newWinStart) ) / 2 )
+  #Now plot with the axis
+  rohAlongChrom = ggplot() + 
+    geom_point(data = newDataFrame, aes(x=newWinStart, y=proportion, color=as.factor(chrom))) +
+    scale_color_manual(values = rep(c(color1, color2), 38 )) +
+    #custom X axis:
+    scale_x_continuous(label = axisdf$chrom, breaks= axisdf$center ) +
+    labs(x = "Chromosome", y = "Fold-change overlapping ROHs (relative to chromosomal mean)") +
+    theme_bw() +
+    theme(axis.text.x = element_text(size=16), 
+          axis.text.y = element_text(size = 16), 
+          axis.title=element_text(size= 18),
+          panel.border = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(), 
+          legend.position = "none")
+  return(rohAlongChrom)
+}
+
+rohAcrossGenome_VCFTools = plotFunction(rescaledWindows,"VCFTools", "#78D7D6","darkmagenta")
+
+rohAcrossGenome_PLINK = plotFunction(rescaledWindows,"PLINK", "royalblue4","darkmagenta")
