b80bf3e58e6bcfad6d76d9ce4222f756e66bf50e
jaam92 <jazlyn.mooney@gmail.com>
2021-09-02 09:35:04 -0700

    reformat script

--- LocalRscripts/Fst/PlotFst.R
+++ LocalRscripts/Fst/PlotFst.R
@@ -10,7 +10,8 @@ df = read.delim("~/DogProject_Jaz/Fst/pairwiseWeighted.fst")
 orderGrEql30 = read.table("~/DogProject_Jaz/Fst/breeds_grEql30.txt")
 
 #make data framewith comparisons the other way from current
-df2 = df %>% select(breed1 = breed2, breed2 = breed1,WeightedFst)
+df2 = df %>% 
+  select(breed1 = breed2, breed2 = breed1,WeightedFst)
 
 #merge together
 mergeDF = rbind.data.frame(df,df2)
@@ -20,7 +21,15 @@ mergeDF$breed1 = factor(mergeDF$breed1, levels=orderGrEql30$V1)
 mergeDF$breed2 = factor(mergeDF$breed2, levels=orderGrEql30$V1)
 
 #plot
-ggplot(data = mergeDF, aes(x=breed1, y=breed2,fill=WeightedFst), colour = "white") +  geom_tile() + scale_fill_gradient(low = "blue", high = "red") + theme_bw() + theme(axis.text.x = element_text(size  = 24,angle=40, vjust=1, hjust=1), axis.text.y = element_text(size  = 24), axis.title=element_text(size=24),legend.title=element_text(size=24), legend.text=element_text(size=18)) 
+ggplot(data = mergeDF, aes(x=breed1, y=breed2,fill=WeightedFst), colour = "white") +  
+  geom_tile() + 
+  scale_fill_gradient(low = "blue", high = "red") + 
+  theme_bw() + 
+  theme(axis.text.x = element_text(size  = 24,angle=40, vjust=1, hjust=1), 
+        axis.text.y = element_text(size  = 24), 
+        axis.title=element_text(size=24),
+        legend.title=element_text(size=24), 
+        legend.text=element_text(size=18)) 
 
 
 #Plot as cladogram
