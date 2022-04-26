e385f76ade422eb5573dd89e1499f2a433524c16
jaam92 <jazlyn.mooney@gmail.com>
2022-04-26 15:34:04 -0700

    added comments

--- LocalRscripts/CaseControlROH/AssociationTest.R
+++ LocalRscripts/CaseControlROH/AssociationTest.R
@@ -1,8 +1,10 @@
+####You will want to replace all the file names and paths here 
+
 #Load libraries
 library(tidyverse)
 library(GMMAT)
 
-#Function to inverse normal transform the score
+#Function to inverse normal transform the ROH score
 qn = function(exp_vector) {
   result = qnorm(rank(exp_vector)/(length(exp_vector)+1))
   return(result)
@@ -11,12 +13,13 @@ qn = function(exp_vector) {
 #Load kinship matrix, ROHs, and phenotype data filename
 pcRelateMat = readRDS("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/rohGRM_allIndivs.rds")
 
+#Load ROH filter to only those greater than or equal to 2MB
 rohs = read.delim(file = "~/Documents/DogProject_Jaz/LocalRscripts/ROH/TrueROH_propCoveredwithin1SDMean_allChroms_mergedFile_Cornell_allChroms_vcfToolsROH_rmROHlessThan50snps_HaywardDataOnly.txt") %>%
   filter(AUTO_LEN >= 2e+06) %>%
   group_by(INDV) %>%
-  summarise(totalROH = sum(as.numeric(AUTO_LEN))) 
+  summarise(totalROH = sum(as.numeric(AUTO_LEN))) #aggregate roh for each individual to get total amount of the genome within a ROH per individual
 
-fnames = paste0("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/IncludeMixedBreeds/", list.files(path="~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/IncludeMixedBreeds/", pattern = "\\.txt$")) #paste the path in front of the filename
+fnames = paste0("~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/IncludeMixedBreeds/", list.files(path="~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/splitPhenotypeFile/IncludeMixedBreeds/", pattern = "\\.txt$")) #grab all the file names from a specific folder and paste the path in front of the filename
 
 #Loop through all files and run association test
 AssociationTestResults = data.frame() #make data frame to store results
@@ -25,7 +28,7 @@ for (i in seq_along(fnames)){
   names(phenotypes)[3] = "trait" #rename third column to trait
   trait = gsub(".*[/]([^.]+)[.txt].*", "\\1", fnames[i]) #keep track of trait and breed tested
   
-  #keep only those rows and columns that correspond to dogs of interest
+  #keep only those rows and columns of kinship matrix that correspond to dogs of interest
   kinshipMat = pcRelateMat %>%
     as.matrix()
   kinshipMat = kinshipMat[, colnames(kinshipMat) %in% phenotypes$dogID]
@@ -36,7 +39,7 @@ for (i in seq_along(fnames)){
     as.data.frame() %>% 
     rename("dogID" = ".") %>%
     mutate(ROHLoad = rohs$totalROH[match(dogID, rohs$INDV)],
-           QNSCORE = qn(as.numeric(ROHLoad)), 
+           QNSCORE = qn(as.numeric(ROHLoad)), #inverse quantile normalize ROH for each trait
            pheno = phenotypes$trait[match(dogID, phenotypes$dogID)],
            status = gsub("1", "0", pheno), 
            status = gsub("2", "1", status),
@@ -48,7 +51,7 @@ for (i in seq_along(fnames)){
     pivot_wider(names_from = status, values_from = n) %>% 
     rename("Case" = "1", "Control" = "0")
   
-  #Run the glmm as a logisitic, compute coeffcients and  p-value  
+  #Run the glmm as a logisitic, compute coeffcients and  p-value 
   out.logistic = glmmkin(status~QNSCORE, data=ROHLoad, kins=kinshipMat*2, id = "dogID", family=binomial(link="logit"))
   LogBeta = out.logistic$coef[2]
   OddsRatio = exp(LogBeta)
@@ -67,10 +70,11 @@ for (i in seq_along(fnames)){
 #write to file
 #write.table(AssociationTestResults, file = "~/Documents/DogProject_Jaz/LocalRscripts/CaseControlROH/AssociationTestResults.txt", col.names = T, row.names = F, quote = F, sep = "\t")
 
+#regular expressions to clean up trait name and make first letter uppercase 
 AssociationTestResults$trait = gsub("(?<=^|_)([a-z])", "\\U\\1", AssociationTestResults$trait, perl=TRUE)
 AssociationTestResults$trait = gsub("_Dog", "_dog", AssociationTestResults$trait, perl = TRUE)
 
-#Plot the output
+#Plot the results
 ggplot(AssociationTestResults, aes(x=gsub("_", " ", trait), y = LogBeta, colour= significant)) +
   geom_hline(yintercept = 0) + 
   geom_errorbar(aes(ymin=lowerBound, ymax=upperBound), colour="gray40", width=.2) + 
